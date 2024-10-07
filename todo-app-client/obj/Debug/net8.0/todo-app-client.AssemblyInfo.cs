72364716fdd11ead49190245a3d3e674377e9930
jilbertvasquez <jilbertvasquez@gmail.com>
2024-10-07 18:29:27 +0800

    clone in pc

--- todo-app-client/obj/Debug/net8.0/todo-app-client.AssemblyInfo.cs
+++ todo-app-client/obj/Debug/net8.0/todo-app-client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("todo-app-client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d85d72c0cda31381f1e1ebaa0e28444f8590f84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4d89dfccaf2c5a3f480f1c7b80f15040f90da5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("todo-app-client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("todo-app-client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
