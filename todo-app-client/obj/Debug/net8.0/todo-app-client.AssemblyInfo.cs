294c05461b11bd64aa4d9c5bc9170bf1e6770961
JilbertVasquez <jilbertvasquez@gmail.com>
2024-10-23 14:27:24 +0800

    run in pc

--- todo-app-client/obj/Debug/net8.0/todo-app-client.AssemblyInfo.cs
+++ todo-app-client/obj/Debug/net8.0/todo-app-client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("todo-app-client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b959e2a16b44079d06e48e76fecc9da5e12a35a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ff1130a13ec4327cc585b807ee480450ca8f9f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("todo-app-client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("todo-app-client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
